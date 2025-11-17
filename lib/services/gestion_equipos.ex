a4c4166b16cc173b204160104c6c8e4d351248f0
dianatijeras <dianam.garciaa1@uqvirtual.edu.co>
2025-11-17 13:35:33 -0500

    feat: funciones listar equipos y agregar integrante en gestion_equipos

--- lib/services/gestion_equipos.ex
+++ lib/services/gestion_equipos.ex
@@ -8,7 +8,7 @@ defmodule Services.GestionEquipos do
   """
 
   alias Domain.Equipo
-
+  alias Adapters.PersistenciaCSV
 
   @doc """
   Crea un nuevo equipo y lo guarda en memoria.
@@ -18,6 +18,35 @@ defmodule Services.GestionEquipos do
     lista = listar_equipos()
     PersistenciaCSV.escribir_equipos([equipo | lista])
     {:ok, equipo}
+  end
+
+  @doc """
+  Lista todos los equipos registrados.
+  """
+  def listar_equipos do
+    PersistenciaCSV.leer_equipos()
+  end
+
+  @doc """
+  Agrega un participante (por ID de usuario) al equipo especificado.
+  """
+  def agregar_integrante(id_equipo, id_participante) do
+    equipos = listar_equipos()
+
+    case Enum.find(equipos, &(&1.id == id_equipo)) do
+      nil -> {:error, "Equipo no encontrado"}
+      equipo ->
+        if Enum.member?(equipo.integrantes, id_participante) do
+          {:error, "El participante ya pertenece a este equipo"}
+        else
+          actualizado = %{equipo | integrantes: [id_participante | equipo.integrantes]}
+          nueva_lista =
+            [actualizado | Enum.reject(equipos, &(&1.id == id_equipo))]
+
+          PersistenciaCSV.escribir_equipos(nueva_lista)
+          {:ok, actualizado}
+        end
+    end
   end
 
 end
