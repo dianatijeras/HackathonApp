f68e09d68e9a08aad9ed31780de508c746514c7c
dianatijeras <dianam.garciaa1@uqvirtual.edu.co>
2025-11-17 13:32:19 -0500

    feat: funciones listar proyectos y agregar avance en gestion_proyectos

--- lib/services/gestion_proyectos.ex
+++ lib/services/gestion_proyectos.ex
@@ -7,6 +7,7 @@ defmodule Services.GestionProyectos do
   """
 
   alias Domain.Proyecto
+  alias Adapters.PersistenciaCSV
 
   @doc """
   Registra un nuevo proyecto asociado a un equipo.
@@ -16,6 +17,37 @@ defmodule Services.GestionProyectos do
     lista = listar_proyectos()
     PersistenciaCSV.escribir_proyectos([proyecto | lista])
     {:ok, proyecto}
+  end
+
+  @doc """
+  Lista todos los proyectos.
+  """
+  def listar_proyectos do
+    PersistenciaCSV.leer_proyectos()
+  end
+
+  @doc """
+  Agrega un avance de progreso a un proyecto existente.
+  """
+  def agregar_avance(id_proyecto, texto) do
+    proyectos = PersistenciaCSV.leer_proyectos()
+
+    case Enum.find(proyectos, fn p -> p.id == id_proyecto end) do
+      nil ->
+        {:error, "Proyecto no encontrado"}
+
+      proyecto ->
+        actualizado = Proyecto.agregar_avance(proyecto, texto)
+
+        nuevos =
+          proyectos
+          |> Enum.reject(&(&1.id == id_proyecto))
+          |> Kernel.++([actualizado])
+
+        PersistenciaCSV.escribir_proyectos(nuevos)
+
+        {:ok, actualizado}
+    end
   end
 
 end
